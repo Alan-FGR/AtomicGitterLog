98837e7b384921339aff999eafa4b116686f76aa
Alan Q <alanqdev@gmail.com>
2017-06-19 17:40:51 -0300

    no message

---
+++ updatedata.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+import json
+
+hist = OrderedDict(json.load(open("log.txt", 'r')))
+
+c=0
+
+gawdsrt = ""
+
+for key in hist:
+    c+=1
+    if(c>300): break;
+    log = hist[key]
+
+    usr = log['user']
+    dat = log['date']
+    msg = log['html']
+
+    if("<img" in log['html']):
+        msg = msg.replace('src=', 'src="loading.PNG" data-src=')
+
+    dat = dat[:10] + "<br>" + dat[11:19]
+
+    gawdsrt += "<tr><td>"+dat+"</td><td>"+usr+"</td><td>"+msg+"</td></tr>"
+
+
+open("index.html", 'w').write(open("template.html", 'r').read().replace("[SHITGOESHERE]", gawdsrt))
