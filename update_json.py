3f0a356dc638dc5d53448577d1ac8dae9300371e
Alan Q <alanqdev@gmail.com>
2017-06-20 10:28:02 -0300

    no message

--- update_json.py
+++ update_json.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
 import json
+from datetime import date
 
-hist = OrderedDict(json.load(open("log.txt", 'r')))
+hist = OrderedDict(json.load(open("../../__atomic_gitter_logs/log.txt", 'r')))
 data = []
+users = []
 
 for key in hist:
     log = hist[key]
@@ -11,19 +13,35 @@ for key in hist:
     dat = log['date']
     msg = log['html']
 
-    if(len(msg) < 4):
+    if usr not in users:
+        users.append(usr)
+
+    if (len(msg) < 4):
         if ':' in msg:
             continue
-	
-    if("<img" in log['html']):
-        msg = msg.replace('src=', 'src="loading.PNG" data-src=')
 
-	dat = dat[:10] + "<br>" + dat[11:19]
-	
+    # TODO UNCOMMENT WHEN YOU CAN MAKE LAZY LOADER WORK :(
+    # if ("<img" in log['html']):
+    #     msg = msg.replace('src=', 'src="loading.PNG" data-src=')
+
+    dat = dat[:10] + "<br>" + dat[11:19]
+
     data.append({
-		'user':usr,
-		'date':dat,
-		'html':msg
-	})
+        'user': usr,
+        'date': dat,
+        'html': msg
+    })
 
-open("log.json", 'w').write(json.dumps(data))#, indent=2))
+open("log.json", 'w').write(json.dumps(data))  # , indent=2))
+
+users.sort()
+users.insert(0,"All")
+users_str = ""
+for u in users:
+    users_str+='<option value="{}">{}</option>\n'.format(u,u)
+
+open("index.html", 'w').write(open("template.html", 'r').read()
+                              .replace("[SHITGOESHERE]", 'Messages: {}, Users: {}, Updated: {}'.format(len(data),len(users),str(date.today())))
+                              .replace("[USERSGOESHERE]", users_str)
+                              .replace("[SIZEGOESHERE]", '{:.2f}'.format(len(json.dumps(data).encode())/1000000))
+                              )
